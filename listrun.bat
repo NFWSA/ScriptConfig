522c0c44a998a8083c2e434fbf30c90facc05090
SurgeNight <surgenight@hotmail.com>
2017-07-25 10:15:48 +0800

    Fix listrun, now it could show all filename include '!' as normal.

--- listrun.bat
+++ listrun.bat
@@ -1,16 +1,13 @@
 @echo off
 set i=0
 pushd "%~dp0"
-setlocal enabledelayedexpansion
 for /f "tokens=1 delims=." %%a in ('dir /b /a-d %1') do (
 	if not "%%a"=="listrun" (
 		set /a i+=1
-		call :print "%%a" 19 !i! 4
-		if !i! equ 4 set i=0
+		call :print "%%a" 20 %%i%% 4
 	)
 )
-call :print " " 19 4 4
-endlocal
+call :print " " 20 4 4
 popd
 echo]
 pause
@@ -22,13 +19,13 @@ rem setlocal enabledelayedexpansion
 set "whitespace= "
 call :strlen %1 size
 set /a size=%2 - %size%
-set dat=!dat!%~1
-for /l %%a in (1, 1, %size%) do set "dat=!dat! "
-if %3 equ %4 (
+set dat=%dat%%~1
+for /l %%a in (1, 1, %size%) do call set "dat=%%dat%% "
+set /a size=%3 %% %4
+if %size% equ 0 (
 	echo]%dat%
 	set "dat="
 )
-rem endlocal
 goto :EOF
 
 :strlen
@@ -36,5 +33,5 @@ rem %1 is str, %2 is return size
 setlocal enabledelayedexpansion
 set "$=%~1#"
 set len=&for %%a in (1024 512 256 128 64 32 16 8 4 2 1)do if !$:~%%a^,1!. NEQ . set/a len+=%%a&set $=!$:~%%a!
-endlocal&set/a%2=%len%
+endlocal&set /a %2=%len%
 goto :EOF
